546043594ef3ce349527760405b3e81b38d871e9
vuhi <vuhi@mail.uc.edu>
2021-05-28 23:32:21 -0400

    add type

--- api_core/apps/utils/response.py
+++ api_core/apps/utils/response.py
@@ -1,10 +1,12 @@
+from typing import Any
+
 from rest_framework.response import Response
 from rest_framework import status
 
 
 class SuccessRes(Response):
 
-    def __init__(self, message: str, data=None, *args, **kwargs):
+    def __init__(self, message: str, data: Any = None, *args, **kwargs):
         super().__init__(data, status.HTTP_200_OK, *args, **kwargs)
         self.data = dict()
         self.data['message'] = message.lower()

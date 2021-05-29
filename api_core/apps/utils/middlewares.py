c1854f6906b38b537e1c1103f006267b01ac9882
vuhi <vuhi@mail.uc.edu>
2021-05-28 23:33:08 -0400

    fix weird response when ValidationError raises

--- api_core/apps/utils/middlewares.py
+++ api_core/apps/utils/middlewares.py
@@ -1,26 +1,34 @@
 import traceback
 
-from django.http import HttpResponse, JsonResponse
+from django.http import JsonResponse
 from rest_framework import status
+from rest_framework.exceptions import ValidationError
 from rest_framework.views import exception_handler
 
 from api_core import settings
 
 
 # Django DRF error handler func. It only handles APIException.
-def drf_custom_exception_handler(exc, context):
+def drf_custom_exception_handler(exception, context):
 
     # Call REST framework's default exception handler first,
-    res = exception_handler(exc, context)
-    if res is not None and isinstance(res.data, dict):
+    res = exception_handler(exception, context)
+    if res is not None:
+        data = dict()
         url = context['request'].build_absolute_uri()
-        res.data['message'] = f'error of type: {type(exc).__name__}, has been thrown at: {url}'
-        res.data['status'] = exc.status_code or status.HTTP_500_INTERNAL_SERVER_ERROR
-        error_msg = res.data.pop('detail', None) or 'unknown error'
-        res.data['error'] = error_msg.lower()
-        if settings.DEBUG:
-            res.data['trace'] = traceback.format_exc()
+        data['message'] = f'error of type: {type(exception).__name__}, has been thrown at: {url}'
+        data['status'] = exception.status_code or status.HTTP_500_INTERNAL_SERVER_ERROR
 
+        if isinstance(exception, ValidationError):
+            details = exception.detail
+            data['error'] = details
+        elif isinstance(res.data, dict) and 'detail' in res.data:
+            data['error'] = res.data.get('detail').lower()
+
+        if settings.DEBUG:
+            data['trace'] = [trace.strip() for trace in traceback.format_exc().split(sep='\n') if trace]
+
+        res.data = data
     return res
 
 

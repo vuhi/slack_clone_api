ce4898727cde986f737bb93c72cde04218b5e712
vuhi <vuhi@mail.uc.edu>
2021-05-28 23:32:34 -0400

    add InvalidLoginCredential

--- api_core/apps/utils/exceptions.py
+++ api_core/apps/utils/exceptions.py
@@ -1,16 +1,25 @@
 from rest_framework import exceptions, status
 
 
-class BadAuthHeader(exceptions.AuthenticationFailed):
-    """ malformed token exception """
+class BadAuthHeader(exceptions.APIException):
+    """ malformed header exception """
     status_code = status.HTTP_401_UNAUTHORIZED
     default_code = 'bad_authorization_header'
 
 
-class InvalidToken(exceptions.AuthenticationFailed):
+class InvalidToken(exceptions.APIException):
+    """ malformed token exception """
     status_code = status.HTTP_401_UNAUTHORIZED
     default_code = 'invalid_token'
 
 
 class TokenError(Exception):
+    """ token exception while decode or sign"""
     pass
+
+
+class InvalidLoginCredential(exceptions.APIException):
+    """ login failed exception """
+    status_code = status.HTTP_401_UNAUTHORIZED
+    default_code = 'bad_credential'
+    default_detail = 'invalid credential, please check your login credentials'
